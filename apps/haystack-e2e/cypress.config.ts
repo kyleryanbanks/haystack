574b6d686b1473928f9fd9fc1783db096e9ffa8d
Kyle Banks <kyle.ryan.banks@gmail.com>
2026-06-13 21:42:14 -0500

    Initial commit

---
+++ apps/haystack-e2e/cypress.config.ts
@@ -0,0 +1,16 @@
+import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
+import { defineConfig } from 'cypress';
+export default defineConfig({
+  e2e: {
+    ...nxE2EPreset(__filename, {
+      cypressDir: 'src',
+      webServerCommands: {
+        default: 'npx nx run haystack:serve',
+        production: 'npx nx run haystack:serve-static',
+      },
+      ciWebServerCommand: 'npx nx run haystack:serve-static',
+      ciBaseUrl: 'http://localhost:4200',
+    }),
+    baseUrl: 'http://localhost:4200',
+  },
+});
